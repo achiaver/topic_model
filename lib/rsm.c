562b5065f238bc41cbba27febcd8c78ebafa93bc
Artur C Grover <artur.turbas@gmail.com>
2022-02-09 22:40:59 -0300

    Update rsm.c code

--- lib/rsm.c
+++ lib/rsm.c
@@ -18,6 +18,6 @@ RSM_t rsm_build(const int nvis, const int nhid)
     rsm_nn.b = (double *) calloc(rsm_nn.nb, sizeof(*rsm_nn.b));
     rsm_nn.v = (int *) calloc(rsm_nn.nv, sizeof(*rsm_nn.v));
     rsm_nn.h = (int *) calloc(rsm_nn.nh, sizeof(rsm_nn.h));
-
+    
     return rsm_nn;
 }
