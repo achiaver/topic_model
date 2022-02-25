ebe0054f969acf92eb40e24bf92ff4b603be9338
Artur C Grover <artur.turbas@gmail.com>
2022-02-24 22:56:10 -0300

    writing visible to hidden computation of contrastive divergence

--- lib/rsm.c
+++ lib/rsm.c
@@ -6,14 +6,27 @@ double sigmoid(const double x)
     return sig;
 }
 
+double * visible_to_hidden(const RMS_t rsm_nn, )
+{
+    double h[rsm_nn.nh] = {0.0};
+    for (int i = 0; i < rsm_nn.nh; i++)
+    {
+        double sum = 0;
+        for (int j = 0; j < rsm_nn.nv; j++)
+        {
+            sum += 
+        }
+    }
+}
 
-RSM_t rsm_build(const int nvis, const int nhid)
+
+RSM_t rsm_build(const int nv, const int nh)
 {
     RSM_t rsm_nn;
-    rsm_nn.nb = nvis + nhid;
-    rsm_nn.nw = nvis * nhid;
-    rsm_nn.nv = nvis;
-    rsm_nn.nh = nhid;
+    rsm_nn.nb = nv + nh;
+    rsm_nn.nw = nv * nh;
+    rsm_nn.nv = nv;
+    rsm_nn.nh = nh;
     rsm_nn.w = (double *) calloc(rsm_nn.nw, sizeof(*rsm_nn.w));
     rsm_nn.b = (double *) calloc(rsm_nn.nb, sizeof(*rsm_nn.b));
     rsm_nn.v = (int *) calloc(rsm_nn.nv, sizeof(*rsm_nn.v));
@@ -24,12 +37,43 @@ RSM_t rsm_build(const int nvis, const int nhid)
 
 void rsm_save(const RSM_t rsm_nn, const char * path)
 {
+    FILE * const file = fopen(path, "w");
+    fprintf(file, "%d %d\n", rsm_nn.nv, rsm_nn.nh);
+    
+    for(int i = 0; i <= rsm_nn.nb; i++)
+    {
+        fprintf(file, "%lf\n", rsm_nn.b[i]);
+    }
 
+    for(int i = 0; i <= rsm_nn.nw; i++)
+    {
+        fprintf(file, "%lf\n", rsm_nn.w[i]);
+    }
+
+    fclose(file);
 }
 
 RSM_t rsm_load(const char * path)
 {
+    FILE * const file = fopen(path, "r");
+    int nv = 0;
+    int nh = 0;
 
+    fscanf(file, "%d %d\n", &nv, &nh);
+
+    const RSM_t rsm_nn = rsm_build(nv, nh);
+
+    for(int i = 0; i <= rsm_nn.nb; i++)
+    {
+        fscanf(file, "%lf\n", &rsm_nn.b[i]);
+    }
+
+    for(int i = 0; i <= rsm_nn.nw; i++)
+    {
+        fscanf(file, "%lf\n", &rsm_nn.w[i]);
+    }
+    fclose(file);
+    return rsm_nn;
 }
 
 void rsm_print(const double * arr, const int size)
@@ -39,5 +83,9 @@ void rsm_print(const double * arr, const int size)
 
 void rsm_free(const RSM_t rsm_nn)
 {
-
+    free(rsm_nn.w);
+    free(rsm_nn.b);
+    free(rsm_nn.v);
+    free(rsm_nn.h);
+    rsm_nn = NULL;
 }
