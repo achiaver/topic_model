b8c5b88e2a05431211aa5d2e41d6e15f78f7bf0f
Artur C Grover <artur.turbas@gmail.com>
2022-03-08 23:15:48 -0300

    Begin coding sample_visible_from_hidden, sampling still need attention

--- libs/rsm.c
+++ libs/rsm.c
@@ -4,70 +4,91 @@
 #include "rsm.h"
 
 
-#define ISEED -42
-
+int iseed = -42;
 
 double sigmoid(const double x) 
 {
     return 1 / (1 + exp(-x));
 }
 
-double * sample_hidden_from_visible(const RSM_t rsm_nn)
+double * sample_hidden_from_visible(const RSM_t *rsm_nn)
 {
-    double *h = calloc(rsm_nn.nh, sizeof(double*));
-    double aux;
-    for (int j = 0; j < rsm_nn.nh; j++)
+    double *h = calloc(rsm_nn->nh, sizeof(double*));
+    for (int j = 0; j < rsm_nn->nh; j++)
     {
-        double sum = 0;
-        for (int i = 0; i < rsm_nn.nv; i++)
+        double sum = 0.0;
+        for (int i = 0; i < rsm_nn->nv; i++)
         {
-            sum += rsm_nn.v[i] * rsm_nn.w[(i * rsm_nn.nh) + j];
+            sum += rsm_nn->v[i] * rsm_nn->w[(i * rsm_nn->nh) + j];
         }
-        h[j] = sigmoid(sum + rsm_nn.b[rsm_nn.nv + j]);
-        if (h[j] >= random_0to1(&ISEED))
+        h[j] = sigmoid(sum + rsm_nn->b[rsm_nn->nv + j]);
+        
+        if (h[j] >= rand_1(&iseed))
+        {
             h[j] = 1.0;
+        }
         else
+        {
             h[j] = 0.0;
+        }
     }
-
     return h;
 }
 
-
-RSM_t rsm_build(const int nv, const int nh)
+double * sample_visible_from_hidden(const RSM_t *rsm_nn)
 {
-    RSM_t rsm_nn;
-    rsm_nn.nb = nv + nh;
-    rsm_nn.nw = nv * nh;
-    rsm_nn.nv = nv;
-    rsm_nn.nh = nh;
-    rsm_nn.w = (double *) calloc(rsm_nn.nw, sizeof(*rsm_nn.w));
-    rsm_nn.b = (double *) calloc(rsm_nn.nb, sizeof(*rsm_nn.b));
-    rsm_nn.v = (int *) calloc(rsm_nn.nv, sizeof(*rsm_nn.v));
-    rsm_nn.h = (int *) calloc(rsm_nn.nh, sizeof(rsm_nn.h));
+    double *v = calloc(rsm_nn->nv, sizeof(double*));
+    for (int i = 0; i < rsm_nn->nv; i++)
+    {
+        double sum = 0.0;
+        for (int j = 0; j < rsm_nn->nh; j++)
+        {
+            sum += rsm_nn->h[j] * rsm_nn->w[(i * rsm_nn->nh) + j];
+        }
+        v[i] = sigmoid(sum + rsm_nn->b[i]);
+
+        if (v[i] >= rand_1(&iseed))
+        {}
+        else
+        {}
+    }
+}
+
+
+RSM_t *rsm_build(const int nv, const int nh)
+{
+    RSM_t *rsm_nn;
+    rsm_nn->nb = nv + nh;
+    rsm_nn->nw = nv * nh;
+    rsm_nn->nv = nv;
+    rsm_nn->nh = nh;
+    rsm_nn->w = (double *) calloc(rsm_nn->nw, sizeof(*rsm_nn->w));
+    rsm_nn->b = (double *) calloc(rsm_nn->nb, sizeof(*rsm_nn->b));
+    rsm_nn->v = (int *) calloc(rsm_nn->nv, sizeof(*rsm_nn->v));
+    rsm_nn->h = (int *) calloc(rsm_nn->nh, sizeof(rsm_nn->h));
     
     return rsm_nn;
 }
 
-void rsm_save(const RSM_t rsm_nn, const char * path)
+void rsm_save(const RSM_t *rsm_nn, const char * path)
 {
     FILE * const file = fopen(path, "w");
-    fprintf(file, "%d %d\n", rsm_nn.nv, rsm_nn.nh);
+    fprintf(file, "%d %d\n", rsm_nn->nv, rsm_nn->nh);
     
-    for(int i = 0; i <= rsm_nn.nb; i++)
+    for(int i = 0; i <= rsm_nn->nb; i++)
     {
-        fprintf(file, "%lf\n", rsm_nn.b[i]);
+        fprintf(file, "%lf\n", rsm_nn->b[i]);
     }
 
-    for(int i = 0; i <= rsm_nn.nw; i++)
+    for(int i = 0; i <= rsm_nn->nw; i++)
     {
-        fprintf(file, "%lf\n", rsm_nn.w[i]);
+        fprintf(file, "%lf\n", rsm_nn->w[i]);
     }
 
     fclose(file);
 }
 
-RSM_t rsm_load(const char * path)
+RSM_t *rsm_load(const char * path)
 {
     FILE * const file = fopen(path, "r");
     int nv = 0;
@@ -75,16 +96,16 @@ RSM_t rsm_load(const char * path)
 
     fscanf(file, "%d %d\n", &nv, &nh);
 
-    const RSM_t rsm_nn = rsm_build(nv, nh);
+    const RSM_t *rsm_nn = rsm_build(nv, nh);
 
-    for(int i = 0; i <= rsm_nn.nb; i++)
+    for(int i = 0; i <= rsm_nn->nb; i++)
     {
-        fscanf(file, "%lf\n", &rsm_nn.b[i]);
+        fscanf(file, "%lf\n", &rsm_nn->b[i]);
     }
 
-    for(int i = 0; i <= rsm_nn.nw; i++)
+    for(int i = 0; i <= rsm_nn->nw; i++)
     {
-        fscanf(file, "%lf\n", &rsm_nn.w[i]);
+        fscanf(file, "%lf\n", &rsm_nn->w[i]);
     }
     fclose(file);
     return rsm_nn;
@@ -95,10 +116,11 @@ void rsm_print(const double * arr, const int size)
 
 }
 
-void rsm_free(RSM_t rsm_nn)
+void rsm_free(RSM_t *rsm_nn)
 {
-    free(rsm_nn.w);
-    free(rsm_nn.b);
-    free(rsm_nn.v);
+    free(rsm_nn->w);
+    free(rsm_nn->b);
+    free(rsm_nn->v);
+    free(rsm_nn);
 }
  
--- libs/rsm.h
+++ libs/rsm.h
@@ -12,11 +12,11 @@ typedef struct rsm
     int nh;     // number of hidden units
 } RSM_t;
 
-RSM_t rsm_build(int nv, int nh);
-void rsm_save(RSM_t rsm_nn, const char * path);
-RSM_t rsm_load(const char * path);
+RSM_t *rsm_build(int nv, int nh);
+void rsm_save(const RSM_t *rsm_nn, const char * path);
+RSM_t *rsm_load(const char * path);
 void rsm_print(const double * arr, int size);
-void rsm_free(RSM_t rsm_nn);
+void rsm_free(RSM_t *rsm_nn);
 
-int * rsm_train(RSM_t rsm_nn, const int * in);
-int * rsm_predict(RSM_t rsm_nn, const int * in);
+int * rsm_train(RSM_t *rsm_nn, const int * in);
+int * rsm_predict(RSM_t *rsm_nn, const int * in);
